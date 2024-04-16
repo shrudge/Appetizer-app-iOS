69c0a583cc37ac6aba861fc9ed61bc9e21f36334
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-17 00:06:54 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Meet Balani on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
