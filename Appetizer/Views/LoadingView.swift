6321abd00d3214abb0487a152fd0adb8a2c173d4
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-06-14 18:00:34 +0530

    Added loading view

---
+++ Appetizer/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Appetizer
+//
+//  Created by Meet Balani on 14/06/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
